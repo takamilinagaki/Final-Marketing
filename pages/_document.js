b1a3d2eb43350f665c74124f09ef7614713e1eab
TakamiInagaki <takami.l.inagaki@gmail.com>
2023-05-16 13:47:24 -0700

    add

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
